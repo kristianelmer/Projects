4b40fa54b208bc5b5009f11d971cf6d15c96777f
Kristian.Elmer <krelm1319@uib.no>
2025-02-07 17:57:04 +0100

    fixed img stuff

--- actuallygood/src/main/GamePanel.java
+++ actuallygood/src/main/GamePanel.java
@@ -11,7 +11,7 @@ public class GamePanel extends JPanel implements Runnable {
     final int originalTileSize = 16;
     final int scale = 3;
 
-    final int tileSize = originalTileSize*scale;
+    public final int tileSize = originalTileSize*scale;
     final int maxScreenCol = 16;
     final int maxScreenRow = 12;
     final int screenWidth = tileSize*maxScreenCol;
@@ -23,11 +23,7 @@ public class GamePanel extends JPanel implements Runnable {
     KeyHandler keyH = new KeyHandler();
     Thread gameThread;
     Player player = new Player(this, keyH);
-    
 
-    int playerX = 100;
-    int playerY = 100;
-    int playerspeed = 4;
     
 
 
@@ -37,7 +33,6 @@ public class GamePanel extends JPanel implements Runnable {
         this.setDoubleBuffered(true);
         this.addKeyListener(keyH);
         this.setFocusable(true);
-
 
     }
 
@@ -49,6 +44,7 @@ public class GamePanel extends JPanel implements Runnable {
 
 
     @Override
+    @SuppressWarnings({"UnusedAssignment", "CallToPrintStackTrace"})
     public void run() {
         while (gameThread != null) {
 
@@ -84,27 +80,16 @@ public class GamePanel extends JPanel implements Runnable {
     }
 
     public void update() {
-        if (keyH.upPressed == true){
-            playerY -= playerspeed;
-        }
-        if (keyH.downPressed == true){
-            playerY += playerspeed;
-        }
-        if (keyH.leftPressed == true){
-            playerX -= playerspeed;
-        }
-        if (keyH.rightPressed == true){
-            playerX += playerspeed;
-        }
+        player.update();
 
     }
 
+    @SuppressWarnings("override")
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
 
         Graphics2D g2 = (Graphics2D)g;
-        g2.setColor(Color.white);
-        g2.fillRect(playerX, playerY, tileSize, tileSize);
+        player.draw(g2);
         g2.dispose();
 
     }
